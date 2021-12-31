fd3948240af2100833208901defb2dabd93d2b54
Dzmitry85 <dpinchuk85@gmail.com>
2021-12-31 16:28:17 +0300

    практическое задание-2

---
+++ lesson-2/dateDiff/js/getDateDiff.js
@@ -0,0 +1,35 @@
+
+import { DateTime } from 'luxon';
+import { printError, printResult } from './printResult.js'
+
+function getDateDiff(dateFrom, dateTo) {
+
+  if (dateFrom < dateTo) {
+    [dateFrom, dateTo] = [dateTo, dateFrom]
+  }
+
+  const dateFromObj = DateTime.fromISO(dateFrom);
+  const dateToObj = DateTime.fromISO(dateTo);
+
+  return dateFromObj.diff(dateToObj, ['years','months', 'days',]).toObject()
+}
+ const form = document.getElementById('datecalc');
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  
+  const dataForm = new FormData(event.target);
+
+  const firstDate = dataForm.get('firstDate');
+  const secondDate = dataForm.get('secondDate');
+
+  if (!firstDate || !secondDate) {
+    printError('Oooopps! Введите дату')
+  } else {
+    const dateREsult = getDateDiff(firstDate, secondDate);
+    printResult(dateREsult);
+  }
+};
+
+
+
